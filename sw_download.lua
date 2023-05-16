8f2ada57cd9ee7ad9a4e29c92d845c0e56c76f3d
jpcoredll <117760970+jpcoredll@users.noreply.github.com>
2023-05-16 07:20:52 +0200

    Update sw_download.lua

--- sw_download.lua
+++ sw_download.lua
@@ -1,1 +1,1 @@
-return "https://www.dropbox.com/s/424rybtzu4u7mzd/com.com2us.smon.normal.freefull.amazon.global.android.common.apk?dl=1"
+return "https://drive.google.com/uc?export=download&id=1WHfqJZf4aOUWH9Xre43vCg_XYISdRFeK"
