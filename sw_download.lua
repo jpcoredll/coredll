4d3601f739df2ff68446a1e5f378577b58cb2c9d
jpcoredll <117760970+jpcoredll@users.noreply.github.com>
2023-10-27 21:17:15 +0200

    Update sw_download.lua

--- sw_download.lua
+++ sw_download.lua
@@ -1,1 +1,1 @@
-return "https://www.dropbox.com/scl/fi/9c38rpcbgm5swnj8bviwz/com.com2us.smon.normal.freefull.amazon.global.android.common.apk?rlkey=b2ovik6ahvr2n1dxtukh129uf&dl=1"
+return "https://www.dropbox.com/scl/fi/qfccmmfo9ealuaep6fvwr/com.com2us.smon.normal.freefull.amazon.global.android.common.apk?rlkey=xwqaue3ka3z1i06wpbd99chem&dl=1"
