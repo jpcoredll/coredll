fb3227ba2a4607006b27c8cb8806f972bc88919e
jpcoredll <117760970+jpcoredll@users.noreply.github.com>
2023-11-02 13:29:19 +0100

    Update sw_download.lua

--- sw_download.lua
+++ sw_download.lua
@@ -1,1 +1,1 @@
-return "https://www.dropbox.com/scl/fi/qfccmmfo9ealuaep6fvwr/com.com2us.smon.normal.freefull.amazon.global.android.common.apk?rlkey=xwqaue3ka3z1i06wpbd99chem&dl=1"
+return "https://www.dropbox.com/scl/fi/j4prgrnfoymeju10ners7/com.com2us.smon.normal.freefull.amazon.global.android.common.apk?rlkey=vb07qis2hoep5fqudonemrp8l&dl=1"
